8bb635918f9c82a113b998c22bb7e104e7afebbf
Ticlero <cbiscuit@naver.com>
2019-09-29 22:38:29 +0900

    모바일 대응 수정

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ ctx.lineWidth = 2.5;
 
 
 let painting = false;
+let touchPainting = false;
 let filling = false;
+let toucthFilling = false;
 
 function startPainting(){
     painting = true;
@@ -32,6 +34,7 @@ function stopPainting(){
 function onMouseMove(evnet){
     const x = event.offsetX;
     const y = event.offsetY;
+    console.log(x,y);
     if(!painting){
         ctx.beginPath();
         ctx.moveTo(x,y);
@@ -50,19 +53,50 @@ function handleCM(event){
     event.preventDefault();
 }
 
+///////////////////////////
+function handleTouchStart(evnet){
+    if(toucthFilling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    const x = event.targetTouches[0].pageX; 
+    const y = event.targetTouches[0].pageY;
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+    touchPainting = true;
+}
+
+function handleTouchEndAndCancel(evnet){
+    touchPainting = false;
+}
+
+function handleTouchMove(event){
+    if(touchPainting){
+        const touchX = event.targetTouches[0].pageX;
+        const touchY = event.targetTouches[0].pageY
+        const offsetX = event.target.offsetLeft;
+        const offsetY = event.target.offsetTop;
+        const x = touchX - offsetX; 
+        const y = touchY - offsetY;
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+///////////////////////////
+
+
 if(canvas)
 {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("touchmove", onMouseMove);
+    canvas.addEventListener("touchmove", handleTouchMove);
 
     canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("touchstart", startPainting);
+    canvas.addEventListener("touchstart", handleTouchStart);
 
     canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("touchend ", stopPainting);
+    canvas.addEventListener("touchend ", handleTouchEndAndCancel);
 
     canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("touchcancel", stopPainting);
+    canvas.addEventListener("touchcancel", handleTouchEndAndCancel);
 
     canvas.addEventListener("click", handleCanvasClick);
     canvas.addEventListener("contextmenu", handleCM);
@@ -85,11 +119,24 @@ function handleRangeContorl(event){
 function handleModeClick(event){
     if(filling === true){
         filling = false;
+        toucthFilling = false;
         mode.innerText = "fill";
     }else{
         filling = true;
+        toucthFilling = false;
         mode.innerText ="paint";
-        ctx.fillStyle
+        //ctx.fillStyle
+    }
+}
+
+function handleModeTouch() {
+    if (filling === true) {
+        toucthFilling = false;
+        mode.innerText = "fill";
+    } else {
+        toucthFilling = true;
+        mode.innerText = "paint";
+        //ctx.fillStyle
     }
 }
 
@@ -107,6 +154,7 @@ if(range){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+    mode.addEventListener("touchstart", handleModeClick);
 }
 
 if(save){
