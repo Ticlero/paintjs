67ec2f224706698e7497ef0e2f4cc6c4f8d4660b
Ticlero <cbiscuit@naver.com>
2019-09-29 21:07:50 +0900

    채우기 저장하기 기능 추가

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.querySelectorAll(".jsColor");
 const range = document.querySelector("#jsRange");
+const mode = document.querySelector("#jsMode");
+const save = document.querySelector("#jsSave");
+const INITAL_COLOR = '#2c2c2c';
+const CANVAS_SIZE = 700;
 
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-canvas.width = 700;
-canvas.height = 700;
-ctx.strokeStyle = '#2c2c2c';
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.strokeStyle = INITAL_COLOR;
+ctx.fillStyle = INITAL_COLOR;
 ctx.lineWidth = 2.5;
 
+
 let painting = false;
-let rangeChanging = false;
+let filling = false;
 
 function startPainting(){
     painting = true;
@@ -32,18 +41,29 @@ function onMouseMove(evnet){
     }
 }
 
+function handleCanvasClick(event){
+    if(filling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+function handleCM(event){
+    event.preventDefault();
+}
+
 if(canvas)
 {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 function handleColorClick(evnet){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
-    console.log(color);
+    ctx.fillStyle = color;
 }
 
 Array.from(colors).forEach( (color) => {
@@ -51,25 +71,55 @@ Array.from(colors).forEach( (color) => {
 });
 
 function handleRangeContorl(event){
-    
+    ctx.lineWidth = event.target.value;
 }
 
-range.addEventListener("mousedown", ()=>{
-    rangeChanging = true;
-});
-
-range.addEventListener("mousemove", (event) =>{
-    if(rangeChanging){
-        const lineWidth = range.value;
-        ctx.lineWidth = lineWidth;
+function handleModeClick(event){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "fill";
+    }else{
+        filling = true;
+        mode.innerText ="paint";
+        ctx.fillStyle
     }
-})
+}
 
-range.addEventListener("mouseup", ()=>{
-    rangeChanging = false;
-});
+function handleSaveClick(event){
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    link.click();
+}
 
-range.addEventListener("click", () =>{
-    const lineWidth = range.value;
-    ctx.lineWidth = lineWidth;
-});
+if(range){
+    range.addEventListener("input", handleRangeContorl);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(save){
+    save.addEventListener("click", handleSaveClick);
+}
+// range.addEventListener("mousedown", ()=>{
+//     rangeChanging = true;
+// });
+
+// range.addEventListener("mousemove", () =>{
+//     if(rangeChanging){
+//         const lineWidth = range.value;
+//         ctx.lineWidth = lineWidth;
+//     }
+// })
+
+// range.addEventListener("mouseup", ()=>{
+//     rangeChanging = false;
+// });
+
+// range.addEventListener("click", () =>{
+//     const lineWidth = range.value;
+//     ctx.lineWidth = lineWidth;
+// });
